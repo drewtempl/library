779b753222bb61c379fefd6a6661ca0e096b93c8
Drew Templeton <templetonag@appstate.edu>
2021-12-28 15:30:48 -0500

    delete button added to cards

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
 
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 }
 
 Book.prototype.info = function() {
@@ -17,13 +18,11 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false, 0);
 addBookToLibrary(hobbit);
-
-const hobbit2 = new Book("Lord of The Rings", "J.R.R. Tolkien", 295, false);
+const hobbit2 = new Book("Lord of The Rings", "J.R.R. Tolkien", 296, false, 1);
 addBookToLibrary(hobbit2);
-
-const hobbit3 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const hobbit3 = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 297, false, 2);
 addBookToLibrary(hobbit3);
 
 const container = document.querySelector('.container');
@@ -49,8 +48,9 @@ function addFields() {
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
+    let id = myLibrary.length;
 
-    let book = new Book(title, author, pages, true);
+    let book = new Book(title, author, pages, true, id);
     addBookToLibrary(book);
     formPopup.classList.toggle('show-form');
     refreshLibrary();
@@ -79,7 +79,7 @@ function refreshLibrary() {
 
 function populateCard(book, card) {
     const title = document.createElement('div');
-    title.textContent = `Title: ${book.title}`
+    title.textContent = `Title:  ${book.title}`
     card.appendChild(title);
 
     const author = document.createElement('div');
@@ -90,11 +90,12 @@ function populateCard(book, card) {
     pages.textContent = `Pages: ${book.pages}`
     card.appendChild(pages);
 
+    card.id = book.id;
+
     const deleteBtn = document.createElement('div');
     deleteBtn.classList.add('deleteBtn')
     deleteBtn.addEventListener('click', () => {
-        let objToRemove = card.parent
-        myLibrary.pop();
+        delete myLibrary[card.id];
         refreshLibrary();
     })
     card.appendChild(deleteBtn);
